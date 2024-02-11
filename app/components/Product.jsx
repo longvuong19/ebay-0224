c732ad20a4e5608e8dfeb8cc2bf0792385937446
longvuong19 <longvuong1920@gmail.com>
2024-02-11 13:46:22 +0700

    add carousel and product component

---
+++ app/components/Product.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+
+const Product = ({ product }) => {
+  return (
+    <>
+      <Link
+        href={`/product/${product.id}`}
+        className="max-w-[200px] p-1.5 border border-gray-50 hover:border-gray-200 hover:shadow-xl bg-gray-100 rounded mx-auto"
+      >
+        {product?.url ? (
+          <img className="rounded cursor-pointer" src={product.url + "/190"} />
+        ) : null}
+
+        <div className="pt-2 px-1">
+          <div className="font-semibold text-[16px] hover:underline cursor-pointer">
+            {product?.title}
+          </div>
+          <div className="font-extrabold">
+            ${(product?.price / 100).toFixed(2)}
+          </div>
+
+          <div className="flex relative items-center text-[12px] text-gray-500">
+            <div className="line-through">
+              {/* fake 20% discount */}$
+              {((product?.price * 1.2) / 100).toFixed(2)}{" "}
+            </div>
+            <div className="px-2">-</div>
+            <div className="line-through">20%</div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default Product;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,41 @@
 "use client";
 
+import CarouselComp from "./components/CarouselComp";
+import Product from "./components/Product";
 import MainLayout from "./layouts/MainLayout";
 
 export default function Home() {
-  return <MainLayout>Test</MainLayout>;
+  const products = [
+    {
+      id: 1,
+      title: "LG Screen",
+      description:
+        "Nulla ultrices tellus id nisl aliquam efficitur. Nulla congue eu libero ut vehicula. Ut faucibus vulputate pharetra. Aenean pretium accumsan magna, in ultricies neque congue in.",
+      url: "https://picsum.photos/id/7",
+      price: 2500,
+    },
+    {
+      id: 2,
+      title: "Snickers",
+      description:
+        "Nulla ultrices tellus id nisl aliquam efficitur. Nulla congue eu libero ut vehicula. Ut faucibus vulputate pharetra. Aenean pretium accumsan magna, in ultricies neque congue in.",
+      url: "https://picsum.photos/id/20",
+      price: 1300,
+    },
+  ];
+
+  return (
+    <MainLayout>
+      <CarouselComp />
+      <div className="max-w-[1200px] mx-auto">
+        <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+
+        <div className="grid grid-cols-5 gap-4">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
